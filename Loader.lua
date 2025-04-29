272776f0350bfcff7982aa98a9e56a02d0d6dcbd
Strixzy <natthapon.guitar@gmail.com>
2025-04-29 20:38:53 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,4 @@ repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 17092550133 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Mxvxrixx/StrixzyHub/refs/heads/main/Scripts/RAMPANTREBORN/Loader.lua"))()
-elseif PlaceId == 94725155944518 or PlaceId == 17282336195 or PlaceId == 17400753636 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Winnable-Hub/Loader/refs/heads/main/Loader.Lua"))()
 end
