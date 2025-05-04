75360331b629ff2b453e2cee7f5e688559040149
Strixzy <natthapon.guitar@gmail.com>
2025-05-05 06:52:49 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,4 +2,6 @@ repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 17092550133 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Mxvxrixx/StrixzyHub/refs/heads/main/Scripts/RAMPANTREBORN/Loader.lua"))()
+elseif PlaceId == 87700573492940 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mxvxrixx/StrixzyHub/refs/heads/main/Scripts/Untitleddrillgame/Loader.lua"))()
 end
